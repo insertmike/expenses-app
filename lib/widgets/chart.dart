50ad602c08183fdf1a7b38db0a6965db19d5482c
Mihail Yonchev <myonchev99@gmail.com>
2019-07-28 20:11:24 +0300

    Added ChartBar Widget

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -20,11 +21,19 @@ class Chart extends StatelessWidget {
           totalSum += recentTransactions[i].amount;
         }
       }
-      print(DateFormat.E().format(weekDay).toString() + " " +  totalSum.toString());
+      print(DateFormat.E().format(weekDay).toString() +
+          " " +
+          totalSum.toString());
       return {
-        'day': DateFormat.E().format(weekDay),
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSum,
       };
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactions.fold(0.0, (sum, el) {
+      return sum + el['amount'];
     });
   }
 
@@ -35,7 +44,15 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: <Widget>[],
+        children: groupedTransactions.map(
+          (el) {
+            return ChartBar(
+              label: el['day'],
+              spendingAmount: el['amount'],
+              spendingPctOfTotal: totalSpending == 0.0 ? 0 : ( ( el['amount'] as double ) / totalSpending ) ,
+            );
+          },
+        ).toList(),
       ),
     );
   }
